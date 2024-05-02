ff45db753075dcced5b6236ebd289cc804c19529
GrandQuad <ocen78@yandex.ru>
2024-05-02 05:59:55 +0300

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.detektiv_laba4"
-        minSdk = 34
+        minSdk = 33
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
